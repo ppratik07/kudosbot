730edc6dcbb3eabdca812fdf8e6b6c5ab87640b3
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-04-13 19:39:59 +0530

    migrating to supabase

---
+++ kudos-bot/prisma/migrations/20250413140920_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Kudos" (
+    "id" TEXT NOT NULL,
+    "senderId" TEXT NOT NULL,
+    "recipientId" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "kudosType" TEXT NOT NULL,
+    "visibility" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Kudos_pkey" PRIMARY KEY ("id")
+);
