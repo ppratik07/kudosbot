c63dcaa45f372d65ca0eb01d6e5baeeba1b68611
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-04-14 11:14:27 +0530

    kudos bot

---
+++ kudosbot/src/bot/kudosBot.ts
@@ -0,0 +1,42 @@
+import { ActivityHandler, BotState, ConversationState, MemoryStorage } from 'botbuilder';
+import { DialogSet, DialogState } from 'botbuilder-dialogs';
+import { SendKudosDialog } from './dialogs/sendKudos';
+import { ViewKudosDialog } from './dialogs/viewKudos';
+
+export class KudosBot extends ActivityHandler {
+  private conversationState: ConversationState;
+  private dialogSet: DialogSet;
+  private dialogState: BotState;
+
+  constructor() {
+    super();
+    // Use in-memory storage for development; replace with Cosmos DB for production
+    const memoryStorage = new MemoryStorage();
+    this.conversationState = new ConversationState(memoryStorage);
+    this.dialogState = this.conversationState.createProperty<DialogState>('DialogState');
+    this.dialogSet = new DialogSet(this.dialogState);
+    this.dialogSet.add(new SendKudosDialog());
+    this.dialogSet.add(new ViewKudosDialog());
+
+    this.onMessage(async (context, next) => {
+      const text = context.activity.text?.toLowerCase().trim();
+      const dialogContext = await this.dialogSet.createContext(context);
+
+      if (text?.includes('send kudos')) {
+        await dialogContext.beginDialog('sendKudosDialog');
+      } else if (text?.includes('view kudos')) {
+        await dialogContext.beginDialog('viewKudosDialog');
+      } else {
+        await context.sendActivity('Try "send kudos" or "view kudos".');
+      }
+
+      await this.conversationState.saveChanges(context);
+      await next();
+    });
+
+    this.onDialog(async (context, next) => {
+      await this.conversationState.saveChanges(context);
+      await next();
+    });
+  }
+}
